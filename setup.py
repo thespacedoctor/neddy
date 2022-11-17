73cbab4c7a1a76d1988cd1fb0beec6726e95eaf0
Dave <d.r.young@qub.ac.uk>
2022-11-17 16:48:38 +0000

    updated docs

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ def readme():
     with open(moduleDirectory + '/README.md') as f:
         return f.read()
 
+
 install_requires = [
     'pyyaml',
     'neddy',
@@ -47,7 +48,7 @@ setup(name="neddy",
       author='David Young',
       author_email='davidrobertyoung@gmail.com',
       license='MIT',
-      packages=find_packages(),
+      packages=find_packages(exclude=["*tests*"]),
       include_package_data=True,
       install_requires=install_requires,
       test_suite='nose2.collector.collector',
